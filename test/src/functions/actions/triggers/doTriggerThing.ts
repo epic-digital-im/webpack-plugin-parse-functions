34463a46c7592217af5b32dc0d41a657be3b009c
chaffinated <dustinchaffin@gmail.com>
2022-09-26 11:25:53 -0700

    task(don't send Parse.Objects over cloud functions)

--- test/src/functions/actions/triggers/doTriggerThing.ts
+++ test/src/functions/actions/triggers/doTriggerThing.ts
@@ -1,5 +1,5 @@
+import { TriggerHandler, TriggerPayloadSchema, ActionTrigger } from '@@functions/helpers';
 import { ServiceConfig, ActionItem } from '@@functions/actions';
-import { TriggerHandler, TriggerPayloadSchema } from '@@functions/helpers';
 import P from 'parse';
 
 type PP = typeof P;
@@ -12,11 +12,9 @@ export const payloadSchema: TriggerPayloadSchema = {
   }
 };
 
-const doTriggerThing: TriggerHandler<ServiceConfig> = (request, Parse, config) => {
-  const payload = request.params;
-  const q = new Parse.Query(payload.get('objectClass'));
-  const obj = q.get(payload.get('objectId'));
-  console.log('do trigger thing', payload, obj);
+const doTriggerThing: TriggerHandler<ServiceConfig> = async (request, Parse, config) => {
+  const { trigger, object } = request.params;
+  console.log('do trigger thing', trigger, object);
 };
 
 export default doTriggerThing;
